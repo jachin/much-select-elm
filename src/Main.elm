b18b17c1ecfe0804ba9577ffda2c7897452e96b6
Jachin Rupe <jachin.rupe@gmail.com>
2020-11-19 01:08:50 -0600

    More markup and adding some styles.

--- src/Main.elm
+++ src/Main.elm
@@ -1,36 +1,108 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, input, text)
-import Html.Attributes exposing (type_)
+import Css exposing (absolute, backgroundColor, color, cursor, display, float, fontSize, height, hex, inlineBlock, left, minWidth, padding, pointer, position, px, relative, rgb, top)
+import Html.Styled exposing (Html, div, input, text, toUnstyled)
+import Html.Styled.Attributes exposing (class, css, type_)
+import Html.Styled.Events exposing (onBlur, onFocus, onMouseOut, onMouseOver)
 
 
 type Msg
-    = NoOp
+    = InputBlur
+    | InputFocus
+    | DropdownMouseOverOption Option
+    | DropdownMouseOutOption Option
 
 
 type alias Model =
     { name : String
     , options : List Option
+    , showDropdown : Bool
     }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        InputBlur ->
+            ( { model | showDropdown = False }, Cmd.none )
+
+        InputFocus ->
+            ( { model | showDropdown = True }, Cmd.none )
+
+        DropdownMouseOverOption option ->
+            ( { model | options = highlightOptionInList option model.options }, Cmd.none )
+
+        DropdownMouseOutOption option ->
+            ( { model | options = removeHighlightOptionInList option model.options }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ input [ type_ "text" ] []
-        , div [] (List.map optionToDropdownOption model.options)
+    div [ css [ position relative ] ]
+        [ input
+            [ type_ "text"
+            , onBlur InputBlur
+            , onFocus InputFocus
+            , css [ height (px 40), fontSize (px 25) ]
+            ]
+            []
+        , dropdown model
         ]
 
 
+dropdown : Model -> Html Msg
+dropdown model =
+    if model.showDropdown then
+        div
+            [ css
+                [ backgroundColor (hex "EEEEEE")
+                , display inlineBlock
+                , padding (px 5)
+                , position absolute
+                , top (px 50)
+                , left (px 0)
+                , fontSize (px 20)
+                , minWidth (px 200)
+                ]
+            ]
+            (List.map optionToDropdownOption model.options)
+
+    else
+        text ""
+
+
 optionToDropdownOption : Option -> Html Msg
-optionToDropdownOption (Option _ _ label _) =
-    div [] [ text (optionLabelToString label) ]
+optionToDropdownOption option =
+    case getOptionDisplay option of
+        OptionShown ->
+            div
+                [ onMouseOver (DropdownMouseOverOption option)
+                , onMouseOut (DropdownMouseOutOption option)
+                , css [ backgroundColor (rgb 255 255 255), padding (px 5), cursor pointer ]
+                ]
+                [ text (getOptionLabelString option) ]
+
+        OptionHidden ->
+            text ""
+
+        OptionSelected ->
+            div
+                [ onMouseOver (DropdownMouseOverOption option)
+                , onMouseOut (DropdownMouseOutOption option)
+                , class "selected"
+                , css [ backgroundColor (hex "111111"), color (hex "EEEEEE"), padding (px 5), cursor pointer ]
+                ]
+                [ text (getOptionLabelString option) ]
+
+        OptionHighlighted ->
+            div
+                [ onMouseOver (DropdownMouseOverOption option)
+                , onMouseOut (DropdownMouseOutOption option)
+                , class "highlighted"
+                , css [ backgroundColor (hex "111111"), color (hex "EEEEEE"), padding (px 5), cursor pointer ]
+                ]
+                [ text (getOptionLabelString option) ]
 
 
 type alias Flags =
@@ -39,14 +111,17 @@ type alias Flags =
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( Model flags.name
-        [ newOption "Red"
-        , newOption "Green"
-        , newOption "Blue"
-        , newOption "Orange"
-        , newOption "Green"
-        , newOption "Purple"
-        ]
+    ( { name = flags.name
+      , options =
+            [ newOption "Red"
+            , newOption "Yellow"
+            , newOption "Orange"
+            , newOption "Green"
+            , newOption "Blue"
+            , newOption "Purple"
+            ]
+      , showDropdown = False
+      }
     , Cmd.none
     )
 
@@ -54,17 +129,14 @@ init flags =
 main : Program Flags Model Msg
 main =
     Browser.element
-        { init = init, update = update, subscriptions = \_ -> Sub.none, view = view }
+        { init = init, update = update, subscriptions = \_ -> Sub.none, view = view >> toUnstyled }
 
 
-type OptionSelected
-    = OptionSelected
-    | OptionNotSelected
-
-
-type OptionVisibility
+type OptionDisplay
     = OptionShown
     | OptionHidden
+    | OptionSelected
+    | OptionHighlighted
 
 
 type OptionLabel
@@ -83,9 +155,55 @@ type OptionValue
 
 
 type Option
-    = Option OptionSelected OptionVisibility OptionLabel OptionValue
+    = Option OptionDisplay OptionLabel OptionValue
 
 
 newOption : String -> Option
 newOption string =
-    Option OptionNotSelected OptionShown (OptionLabel string) (OptionValue string)
+    Option OptionShown (OptionLabel string) (OptionValue string)
+
+
+getOptionDisplay : Option -> OptionDisplay
+getOptionDisplay (Option display _ _) =
+    display
+
+
+getOptionLabelString : Option -> String
+getOptionLabelString (Option _ label _) =
+    optionLabelToString label
+
+
+highlightOptionInList : Option -> List Option -> List Option
+highlightOptionInList option options =
+    List.map
+        (\o ->
+            if o == option then
+                highlightOption option
+
+            else
+                o
+        )
+        options
+
+
+removeHighlightOptionInList : Option -> List Option -> List Option
+removeHighlightOptionInList option options =
+    List.map
+        (\o ->
+            if o == option then
+                removeHighlightOption option
+
+            else
+                o
+        )
+        options
+
+
+highlightOption : Option -> Option
+highlightOption (Option _ label value) =
+    Option OptionHighlighted label value
+
+
+removeHighlightOption : Option -> Option
+removeHighlightOption (Option _ label value) =
+    Option OptionShown label value
