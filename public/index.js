e450b59ec7cb7548a391eadbbb7a585ed770dd79
Jachin Rupe <jachin.rupe@gmail.com>
2020-12-28 00:45:28 -0600

    Adding a nicer event logger for demos.

--- public/index.js
+++ public/index.js
@@ -1,7 +1,11 @@
 import MuchSelector from "../src/much-select.js";
 
+import makeLogEventHandler from "./event-logger.js";
+
 if (!customElements.get("much-select")) {
   // Putting guard rails around this because browsers do not like
   //  having the same custom element defined more than once.
   window.customElements.define("much-select", MuchSelector);
 }
+
+window.makeLogEventHandler = makeLogEventHandler;
