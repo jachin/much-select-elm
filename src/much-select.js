f84d553b981fd85019fb591388c8f52840546762
Jachin Rupe <jachin.rupe@drip.com>
2021-02-23 00:51:35 -0600

    Taking care of a couple of TODOs.

--- src/much-select.js
+++ src/much-select.js
@@ -596,15 +596,13 @@ class MuchSelect extends HTMLElement {
       // TODO perhaps this delimiter should be configurable
       const values = value.split(",");
 
-      // TODO Convert this._app to a function that get a promise that returns _app
-      //  when it is ready.
       // noinspection JSUnresolvedVariable
-      this._app.ports.valueChangedReceiver.send(values);
+      this.appPromise.then((app) =>
+        app.ports.valueChangedReceiver.send(values)
+      );
     } else {
-      // TODO Convert this._app to a function that get a promise that returns _app
-      //  when it is ready.
       // noinspection JSUnresolvedVariable
-      this._app.ports.valueChangedReceiver.send([]);
+      this.appPromise.then((app) => app.ports.valueChangedReceiver.send([]));
     }
   }
 
