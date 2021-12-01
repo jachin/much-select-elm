7b5ba5350ac585af4aa2dc14f1997ab3c71e3789
Jachin Rupe <jachin.rupe@drip.com>
2021-12-01 10:32:17 -0600

    Try different sed command because github actions is probably linux.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,15 @@
 
 npx elm-esm make src/Main.elm --output=dist/elm-main.js --optimize
 cp ./src/*.js ./dist/
-sed -i '' -e 's/Main\.elm/elm-main\.js/g' ./dist/much-select.js
-
+if [[ -z "${CI}" ]]; then
+  sed -i -e 's/Main\.elm/elm-main\.js/g' ./dist/much-select.js
+else
+  sed -i '' -e 's/Main\.elm/elm-main\.js/g' ./dist/much-select.js
+fi
 npx elm-esm make src/Main.elm --output=dist/elm-main-debug.js --debug
 cp ./src/much-select.js ./dist/much-select-debug.js
-sed -i '' -e 's/Main\.elm/elm-main-debug\.js/g' ./dist/much-select-debug.js
+if [[ -z "${CI}" ]]; then
+  sed -i -e 's/Main\.elm/elm-main-debug\.js/g' ./dist/much-select-debug.js
+else
+  sed -i '' -e 's/Main\.elm/elm-main-debug\.js/g' ./dist/much-select-debug.js
+fi
