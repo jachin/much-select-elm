7c81e0f009fbaab15fffcfab1330aa88df2af766
Jachin Rupe <jachin.rupe@drip.com>
2021-12-01 10:00:42 -0600

    Maybe this will fix the build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,8 @@
 
 npx elm-esm make src/Main.elm --output=dist/elm-main.js --optimize
 cp ./src/*.js ./dist/
-sed -i '' -e 's/Main\.elm/elm-main\.js/g' dist/much-select.js
+sed -i '' -e 's/Main\.elm/elm-main\.js/g' ./dist/much-select.js
 
 npx elm-esm make src/Main.elm --output=dist/elm-main-debug.js --debug
 cp ./src/much-select.js ./dist/much-select-debug.js
-sed -i '' -e 's/Main\.elm/elm-main-debug\.js/g' dist/much-select-debug.js
+sed -i '' -e 's/Main\.elm/elm-main-debug\.js/g' ./dist/much-select-debug.js
