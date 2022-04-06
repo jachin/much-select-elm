35e088acff7b2b7f57b5f5a71235a0458a2dd23a
Jachin Rupe <jachin.rupe@drip.com>
2022-04-06 16:43:06 -0500

    Adding a example to help sort this out. I think things are mostly working.
    There were some extra events getting fired when the value was cleared.

--- public/event-logger.js
+++ public/event-logger.js
@@ -1,24 +1,25 @@
-const makeLogEventHandler = (logElementId) => {
-  const logEvent = (event) => {
-    const detailStr = JSON.stringify(event.detail);
-    const tr = document.createElement("tr");
-    const eventElementId = `${event.type}-${event.timeStamp}`;
-    tr.setAttribute("id", eventElementId);
-    tr.innerHTML = `
+const makeLogEventHandler = (logElementId) => (event) => {
+  const detailStr = JSON.stringify(event.detail);
+  const tr = document.createElement("tr");
+  const eventElementId = `${event.type}-${event.timeStamp}`;
+
+  // eslint-disable-next-line no-console
+  console.info(event);
+
+  tr.setAttribute("id", eventElementId);
+  tr.innerHTML = `
     <td>
       <strong><code>${event.type}</code></strong>
     </td>
     <td><code>${detailStr}</code></td>`;
-    document.getElementById(logElementId).querySelector("tbody").prepend(tr);
+  document.getElementById(logElementId).querySelector("tbody").prepend(tr);
 
-    window.setTimeout(() => {
-      const logItemNode = document.getElementById(eventElementId);
-      if (logItemNode.parentNode) {
-        logItemNode.parentNode.removeChild(logItemNode);
-      }
-    }, 5000);
-  };
-  return logEvent;
+  window.setTimeout(() => {
+    const logItemNode = document.getElementById(eventElementId);
+    if (logItemNode.parentNode) {
+      logItemNode.parentNode.removeChild(logItemNode);
+    }
+  }, 5000);
 };
 
 export default makeLogEventHandler;
