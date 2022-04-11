9c7b64f0ad16685a5bf6edf1970aad3045c2e1c7
Jachin Rupe <jachin.rupe@drip.com>
2022-04-11 12:32:59 -0500

    Changing how the weighting is done.

--- src/OptionSearchFilter.elm
+++ src/OptionSearchFilter.elm
@@ -1,4 +1,4 @@
-module OptionSearchFilter exposing (OptionSearchFilter, OptionSearchResult, getLowScore, impossiblyLowScore, lowScoreCutOff, new)
+module OptionSearchFilter exposing (OptionSearchFilter, OptionSearchResult, impossiblyLowScore, new)
 
 import Fuzzy exposing (Result)
 
@@ -28,38 +28,9 @@ impossiblyLowScore =
 new : Int -> Int -> OptionSearchResult -> List ( Bool, String ) -> List ( Bool, String ) -> List ( Bool, String ) -> OptionSearchFilter
 new totalScore bestScore searchResult labelTokens descriptionTokens groupTokens =
     { totalScore = totalScore
-    , bestScore = totalScore
+    , bestScore = bestScore
     , searchResult = searchResult
     , labelTokens = labelTokens
     , descriptionTokens = descriptionTokens
     , groupTokens = groupTokens
     }
-
-
-getLowScore : OptionSearchResult -> Int
-getLowScore optionSearchResult =
-    List.minimum
-        [ optionSearchResult.labelMatch.score
-        , optionSearchResult.descriptionMatch.score
-        , optionSearchResult.groupMatch.score
-        ]
-        |> Maybe.withDefault
-            impossiblyLowScore
-
-
-lowScoreCutOff : Int -> Int
-lowScoreCutOff score =
-    if score == 0 then
-        10
-
-    else if score <= 10 then
-        100
-
-    else if score <= 100 then
-        1000
-
-    else if score <= 1000 then
-        10000
-
-    else
-        impossiblyLowScore
