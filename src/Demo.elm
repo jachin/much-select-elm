70a8c4446340c0e95d5a114dd7df5fad4fcc432c
Jachin Rupe <jachin.rupe@drip.com>
2022-05-18 15:10:09 -0500

    Improving the elm demo.

--- src/Demo.elm
+++ src/Demo.elm
@@ -1,7 +1,10 @@
 module Demo exposing (main)
 
 import Browser
-import Html exposing (Html)
+import Html exposing (Attribute, Html, button, div, option, select, text)
+import Html.Attributes exposing (attribute)
+import Html.Attributes.Extra
+import Html.Events exposing (onClick)
 
 
 type alias Flags =
@@ -9,11 +12,12 @@ type alias Flags =
 
 
 type alias Model =
-    ()
+    { allowCustomOptions : Bool }
 
 
 type Msg
     = ValueChanged (List String)
+    | ToggleAllowCustomValues
 
 
 main : Program Flags Model Msg
@@ -28,7 +32,7 @@ main =
 
 init : Flags -> ( Model, Cmd Msg )
 init _ =
-    ( (), Cmd.none )
+    ( { allowCustomOptions = False }, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -37,12 +41,39 @@ update msg model =
         ValueChanged strings ->
             ( model, Cmd.none )
 
+        ToggleAllowCustomValues ->
+            ( { model | allowCustomOptions = not model.allowCustomOptions }, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
 
 
+slot : String -> Attribute msg
+slot string =
+    attribute "slot" string
+
+
+allowCustomOptionsAttribute : Bool -> Attribute msg
+allowCustomOptionsAttribute bool =
+    if bool then
+        attribute "allow-custom-options" ""
+
+    else
+        Html.Attributes.Extra.empty
+
+
 view : Model -> Html Msg
 view model =
-    Html.node "much-select" [] []
+    div []
+        [ Html.node "much-select"
+            [ allowCustomOptionsAttribute model.allowCustomOptions ]
+            [ select [ slot "select-input" ]
+                [ option [] [ text "Tom" ]
+                , option [] [ text "Bert" ]
+                , option [] [ text "William" ]
+                ]
+            ]
+        , button [ onClick ToggleAllowCustomValues ] [ text "toggle allow custom values" ]
+        ]
