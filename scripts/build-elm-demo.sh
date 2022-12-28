5eeccfca499c56c8644a9674d395313b8ac0d505
Jachin Rupe <jachin@jachin.rupe.name>
2022-12-28 13:12:37 -0600

    Adding some more helpful comments.

--- scripts/build-elm-demo.sh
+++ scripts/build-elm-demo.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This a script for building the Elm demo App. Since we want much-select to
+# work well as part of a larger Elm app, it's important that we're able
+# to test the featuers of much-select in Elm's virtual DOM's.
+
 # Ensure the build directory is there
 mkdir -p ./build
 
@@ -9,4 +13,5 @@ npx elm-esm make ./src/Demo.elm --output=./build/elm-demo.js --debug
 # together it easy to see where 1 starts and the other ends.
 printf "\n\n" >> ./build/elm-demo.js
 
+# Put together the compiled Elm and JavaScript to start up the app.
 cat ./src/elm-demo.js >> ./build/elm-demo.js
