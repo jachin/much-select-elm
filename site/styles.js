1023b2c394bf3491dcb513fb549b861d892f6c12
Jachin Rupe <jachin.rupe@drip.com>
2023-05-31 12:39:41 -0500

    Getting the theme switching to an even better place.

--- site/styles.js
+++ site/styles.js
@@ -1,26 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const themes = ["loading", "fun", "boring"];
-
-  const setTheme = (newTheme) => {
-    const body = document.querySelector("body");
-    themes.map((theme) => body.classList.remove(theme));
-    body.classList.add(newTheme);
-    localStorage.setItem("page-theme", newTheme);
-  };
-
-  // Setup handlers on the buttons for switching themes
-  document.getElementById("fun-style").addEventListener("click", () => {
-    setTheme("fun");
-  });
-  document.getElementById("boring-style").addEventListener("click", () => {
-    setTheme("boring");
-  });
-
-  // Set the initial page theme
-  let initialPageTheme = localStorage.getItem("page-theme");
-  if (!initialPageTheme) {
-    // default theme
-    initialPageTheme = "fun";
-  }
-  setTheme(initialPageTheme);
+// Setup handlers on the buttons for switching themes
+document.getElementById("fun-style").addEventListener("click", () => {
+  document.getElementById("theme-switch-fun").checked = true;
+  localStorage.setItem("page-theme", "fun");
 });
+document.getElementById("boring-style").addEventListener("click", () => {
+  document.getElementById("theme-switch-boring").checked = true;
+  localStorage.setItem("page-theme", "boring");
+});
+
+// Set the initial page theme
+const initialPageTheme = localStorage.getItem("page-theme");
+if (!initialPageTheme) {
+  // default theme
+  document.getElementById("theme-switch-boring").checked = true;
+} else if (initialPageTheme === "fun") {
+  document.getElementById("theme-switch-fun").checked = true;
+} else if (initialPageTheme === "boring") {
+  document.getElementById("theme-switch-boring").checked = true;
+}
