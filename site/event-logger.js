fa4a754c796001229fc71ab8409f43562f453b7a
Jachin Rupe <jachin@jachin.rupe.name>
2024-11-14 10:17:09 -0600

    Starting work on the event styling with the `muchSelectReady` event.
    Renaming some things, because prefixing things with `much-select` is
    silly sometimes.
    
    Starting work on styling the table for showing the events.

--- site/event-logger.js
+++ site/event-logger.js
@@ -13,11 +13,23 @@ const makeLogEventHandler = (logElementId) => (event) => {
     </td>
     <td><pre><code>${detailStr}</code></pre></td>`;
   document.getElementById(logElementId).querySelector("tbody").prepend(tr);
+  document
+    .getElementById(logElementId)
+    .querySelector(".event-log-empty-row").style.display = "none";
 
   window.setTimeout(() => {
     const logItemNode = document.getElementById(eventElementId);
+    const parentElement = logItemNode.parentNode;
     if (logItemNode.parentNode) {
       logItemNode.parentNode.removeChild(logItemNode);
+    }
+
+    const noRecentEventsToShow =
+      parentElement.querySelectorAll("tr:not(.event-log-empty-row)").length < 1;
+    if (noRecentEventsToShow) {
+      document
+        .getElementById(logElementId)
+        .querySelector(".event-log-empty-row").style.display = "block";
     }
   }, 10000);
 };
